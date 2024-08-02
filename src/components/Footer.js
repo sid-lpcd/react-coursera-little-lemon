54f71e618436a356d3fac967c2cd00e06d4884f4
sid-lpcd <87264085+sid-lpcd@users.noreply.github.com>
2024-08-02 10:07:18 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,34 @@
 function Footer() {
   return (
     <footer>
+      <img src='../images/logo.png'></img>
+      <div>
+        <p class='title'>Footer Navigation<p/>
+        <ul>
+          <li><a href=''>Home</a></li>
+          <li><a href=''>About</a></li>
+          <li><a href=''>Menu</a></li>
+          <li><a href=''>Reservations</a></li>
+          <li><a href=''>Order Online</a></li>
+          <li><a href=''>Login</a></li>
+        </ul>
+      </div>
+      <div>
+        <p class='title'>Contact<p/>
+        <ul>
+          <li><a href=''>Address</a></li>
+          <li><a href=''>Phone Number</a></li>
+          <li><a href=''>Email</a></li>
+        </ul>
+      </div>
+      <div>
+        <p class='title'>Social Media Links<p/>
+        <ul>
+          <li><a href=''>Instagram</a></li>
+          <li><a href=''>Facebook</a></li>
+          <li><a href=''>LinkedIn</a></li>
+        </ul>
+      </div>
     </footer>
   );
 }
