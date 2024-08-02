f56b02a553617f663ea8063e9d738b86854fc55e
sid-lpcd <87264085+sid-lpcd@users.noreply.github.com>
2024-08-02 10:02:25 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,15 @@
 function Nav() {
   return (
     <nav>
+      <img src='../images/logo.png'></img>
+      <ul>
+        <li><a href=''>Home</a></li>
+        <li><a href=''>About</a></li>
+        <li><a href=''>Menu</a></li>
+        <li><a href=''>Reservations</a></li>
+        <li><a href=''>Order Online</a></li>
+        <li><a href=''>Login</a></li>
+      </ul>
     </nav>
   );
 }
