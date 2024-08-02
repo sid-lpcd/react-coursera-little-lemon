e01a951be6318b183b5028a3f20343fe581af62e
sid-lpcd <87264085+sid-lpcd@users.noreply.github.com>
2024-08-02 10:18:20 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 function Header() {
   return (
     <header>
+      <meta name="description" content="Enjoy a delightful dining experience at Little Lemon Restaurant, where fresh ingredients and vibrant flavors come together in a cozy, inviting atmosphere. Discover our unique menu featuring zesty lemon-infused dishes, crafted to perfection by our talented chefs. Perfect for family dinners, romantic dates, and special occasions. Visit us for a refreshing culinary journey!"/>
+      <meta name="og:title" content="Little Lemon Restaurant"/>
+      <meta name="og:description" content="Enjoy a delightful dining experience at Little Lemon Restaurant, where fresh ingredients and vibrant flavors come together in a cozy, inviting atmosphere."/>
+      <meta name="og:image" content="../images/dishes.png"/>
     </header>
   );
 }
