de0ff6e6aefc9c75278f0bd7e2ef27cfaa5e3fdb
Sid <sidoniomiguelsilva567@gmail.com>
2024-10-16 09:44:27 +0100

    Add testimonial card styling

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,14 +1,14 @@
 function TestimonialCard(props) {
-    return (
-      <div className="card col-2 flex-column  justify-content-center">
-        <h5 className="card-title">{props.title}</h5>
-        <div className="row align-items-center">
-          <img src={props.img} className="card-img-side w-50"></img>
-          <p>{props.text_profile}</p>
-        </div>
-        <p className="card-text">{props.text_review}</p>
+  return (
+    <div className="card col-2 flex-column  justify-content-center">
+      <h5 className="card-title">{props.title}</h5>
+      <div className="row align-items-center">
+        <img src={props.img} className="card-img-side w-50"></img>
+        <p className="card-profile">{props.text_profile}</p>
       </div>
-    );
-  }
-  
-  export default TestimonialCard;
+      <p className="card-text">{props.text_review}</p>
+    </div>
+  );
+}
+
+export default TestimonialCard;
