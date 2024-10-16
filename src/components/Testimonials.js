03aa393c6b1b2a25a1777a5c38a599b993eeec81
sid-lpcd <87264085+sid-lpcd@users.noreply.github.com>
2024-10-16 09:29:21 +0100

    Update Testimonials.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,18 +1,51 @@
 import TestimonialCard from "./TestimonialCard";
-import image from "../images/restaurant chef B.jpg";
+import image1 from "../images/people1.jpg";
+import image2 from "../images/people2.jpg";
+import image3 from "../images/people3.jpg";
+import image4 from "../images/people4.jpg";
+
 function Testimonials() {
-    const testimonialList = [];
-    for (let i = 0; i < 4; i++) {
-        testimonialList.push(
-            <TestimonialCard key={i} img={image} title={"Rating"} text_profile={"Profile"} text_review={"Review"} />
-        );
+
+    const testimonialList = [
+    {
+      img: image1,
+      title: "Excellent Service",
+      text_profile: "John Doe, Food Blogger",
+      text_review: "The food was fantastic and the service was impeccable. Will definitely come again!"
+    },
+    {
+      img: image2,
+      title: "Wonderful Experience",
+      text_profile: "Jane Smith, Travel Enthusiast",
+      text_review: "A truly unforgettable dining experience with exceptional ambiance and delightful dishes."
+    },
+    {
+      img: image3,
+      title: "Top-Notch Quality",
+      text_profile: "Michael Brown, Chef",
+      text_review: "Every dish was masterfully prepared, and the ingredients were top-notch. Highly recommend!"
+    },
+    {
+      img: image4,
+      title: "Great Atmosphere",
+      text_profile: "Harry David, Restaurant Critic",
+      text_review: "The restaurant had such a warm and inviting atmosphere. The staff was friendly and the food was delicious!"
     }
+  ];
   return (
     <section className="container container-green flex-column" id="testimonials">
         <h1 className="title-primary">Testimonials</h1>
         <div className="row justify-content-center">
-            {testimonialList}
-        </div>
+        {testimonialList.map((testimonial, index) => (
+          <TestimonialCard
+            key={index}
+            img={testimonial.img}
+            title={testimonial.title}
+            text_profile={testimonial.text_profile}
+            text_review={testimonial.text_review}
+          />
+        ))}
+      </div>
     </section>
 
   );
